bab3778f472affedd73a3796ba66eaccba1c8e1c
ali <zboom0ne01@gmail.com>
2026-02-24 15:43:58 +0330

    update

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -12,7 +12,7 @@ export default function Header() {
         <header className="fixed top-0 left-0 right-0 z-50 bg-[#0A1F44]/30 backdrop-blur-lg border-b border-white/10 shadow-sm">
             <div className="mx-auto px-20 md:px-10 py-4 flex items-center justify-between">
                 {/* لوگو / عنوان */}
-                <Link href="/" className="text-2xl flex items-center font-bold tracking-tight">
+                <Link href="/" className="text-2xl flex items-center text-white font-bold tracking-tight">
                     <DynamicSlaIcon className=" ml-4" />
                     Dynamic<span className="text-[#FF6B00]">SLA</span>
                 </Link>
